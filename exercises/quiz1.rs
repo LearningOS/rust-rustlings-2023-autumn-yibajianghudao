dc96fa0410aa08fb07ee8a8be4e769f55f9cb970
yibajianghudao <w2027075535@outlook.com>
2023-10-09 19:34:52 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32) -> i32 {
+    let mut price :i32 = 0;
+    if num <= 40 {
+        price = num * 2;
+    } else {
+        price = num;
+    }
+    price
+}
 
 // Don't modify this function!
 #[test]
